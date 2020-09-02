456f5d56a4054d69c896e2cbd6595ba80e8afd68
dung6eetle <tem09970967@gmail.com>
2020-09-02 16:57:16 +0300

    added Slider

---
+++ main.js
@@ -0,0 +1,34 @@
+const prev = document.getElementById("btn-prev"),
+      next = document.getElementById("btn-next"),
+      slides = document.querySelectorAll(".slide"),
+      slidesWrapper = document.querySelectorAll(".slider-wrapper");
+console.log('slide', slides)
+let index = 0;
+
+const activeSlide = n => {
+    console.log(n)
+    for(slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0
+        activeSlide(index);
+    } else {
+        index++;
+        activeSlide(index);
+    }
+}
+const prevSlide = () => {
+    if(index == 0) {
+        index = slides.length - 1
+        activeSlide(index);
+    } else {
+        index--;
+        activeSlide(index);
+    }
+}
+next.addEventListener('click', nextSlide)
+prev.addEventListener('click', prevSlide)
